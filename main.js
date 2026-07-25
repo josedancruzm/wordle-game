81c793093d2f9ad20fa6b735483503c9004fa962
josedancruzm <jose.cruz190@upr.edu>
2026-07-25 15:55:43 -0400

    added wrong word animation

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ document.addEventListener('keydown', (event) => {
     if (/^[A-Z]$/.test(key)) {
         if (cell < 5) {
             allCells[index].textContent = key;
+            allCells[index].classList.add('inserted');
             cell++;
         }
     }
@@ -20,7 +21,7 @@ document.addEventListener('keydown', (event) => {
         if (cell > 0) {
             cell--;
             allCells[index - 1].textContent = '';
-
+            allCells[index-1].classList.remove('inserted');
         }
     }
 
@@ -35,12 +36,6 @@ document.addEventListener('keydown', (event) => {
             alert("The word was " + chosen_word + "!");
         }
     }
-
-    if (allCells[index].textContent !== '') {
-        allCells[index].classList.add('inserted');
-    } else{
-        allCells[index-1].classList.remove('inserted');
-    }
 })
 
 function wordCheker() {
@@ -52,7 +47,11 @@ function wordCheker() {
 
     //checks if the word is in the word bank
     if (!word_bank.includes(guess)) {
-        alert("Word does not exist!");
+        for (let i = 0; i < 5; i++) {
+            allCells[row * 5 + i].classList.remove('incorrect');
+            void  allCells[row * 5 + i].offsetWidth;
+            allCells[row * 5 + i].classList.add('incorrect');
+        }
         cell = 5;
         return;
     }
