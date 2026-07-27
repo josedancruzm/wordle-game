e7a3718f9f2fa37615231baf163dd2e040a0111e
josedancruzm <jose.cruz190@upr.edu>
2026-07-27 14:44:24 -0400

    added animation for title!

--- header.js
+++ header.js
@@ -5,7 +5,7 @@ const title = ["W", "O", "R", "D", "L", "E"]
 //block that gives each column its own staggered floating animation
 allTitleCellWrappers.forEach((element, id) => {
     element.classList.add('floating');
-    element.style.animationDelay = ((id % 6) * 200)+ 'ms';
+    element.style.animationDelay = ((id % 6) * 200) + 'ms';
 });
 
 for (let i = 0; i < 6; i++) {
@@ -13,9 +13,20 @@ for (let i = 0; i < 6; i++) {
         allTitleCells[i].textContent = title[i];
         allTitleCells[i].classList.add('inserted');
         allTitleCells[i].classList.add('hiccup-title');
-    }, (i*200))
-        
+
+    }, (i * 200))
 }
 
+let duration = 5 * 200;
 
+setTimeout(() => {
+    for (let i = 0; i < 6; i++) {
 
+        setTimeout(() => {
+            allTitleCells[i].classList.add('flip');
+        }, (i*200))
+        setTimeout(() => {
+            allTitleCells[i].classList.add('correct');
+        }, ((i*200)+500))
+    }
+}, (duration + 400))
