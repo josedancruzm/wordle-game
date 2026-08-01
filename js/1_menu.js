fb793f196f906a77e64d2b99c82417bde31a43e4
josedancruzm <jose.cruz190@upr.edu>
2026-08-01 19:29:41 -0400

    added score counter for ladder mode

--- js/1_menu.js
+++ js/1_menu.js
@@ -7,9 +7,21 @@ allButtonWrappers.forEach((element, id) => {
 
 const menu = document.querySelector('.menu-grid');
 const game = document.querySelector('.game-grid');
-const classic = document.querySelector('.classic');
 
-classic.addEventListener('click', () => {
+const classic_button = document.querySelector('.classic');
+const ladder_button = document.querySelector('.ladder');
+
+
+classic_button.addEventListener('click', () => {
     menu.style.display = 'none'
     game.style.display = 'flex'
+    document.querySelector('.level-counter').style.display = 'none';
+    document.querySelector('.highscore-counter').style.display = 'none';
+});
+
+ladder_button.addEventListener('click', () => {
+    menu.style.display = 'none'
+    game.style.display = 'flex'
+    document.querySelector('.level-counter').style.display = 'flex';
+    document.querySelector('.highscore-counter').style.display = 'flex';
 });
